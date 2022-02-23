d36e22dfcd564ab632cd173aff949e7cb840caa0
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-02-23 20:00:29 +0900

    feat: add drawer and fcm token confirmation widget

---
+++ packages/mobile/lib/theme/theme.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+final grey50 = Colors.grey[50]!;
+final grey100 = Colors.grey[100]!;
+final grey200 = Colors.grey[200]!;
+final grey400 = Colors.grey[400]!;
+final grey500 = Colors.grey[500]!;
+final grey600 = Colors.grey[600]!;
+final grey700 = Colors.grey[700]!;
+
+/// テキストスタイル
+const TextStyle bold = TextStyle(
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle semibold = TextStyle(
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle regular10 = TextStyle(
+  fontSize: 10,
+);
+
+const TextStyle regular12 = TextStyle(
+  fontSize: 12,
+);
+
+const TextStyle regular14 = TextStyle(
+  fontSize: 14,
+);
+
+const TextStyle regular16 = TextStyle(
+  fontSize: 16,
+);
+
+const TextStyle bold12 = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle bold14 = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle bold16 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle bold20 = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+
+final TextStyle grey10 = TextStyle(
+  fontSize: 10,
+  color: Colors.grey[600],
+);
+
+final TextStyle grey12 = TextStyle(
+  fontSize: 12,
+  color: Colors.grey[600],
+);
+
+final TextStyle grey14 = TextStyle(
+  fontSize: 14,
+  color: Colors.grey[600],
+);
+
+final TextStyle greyBold12 = TextStyle(
+  fontSize: 12,
+  color: Colors.grey[600],
+  fontWeight: FontWeight.bold,
+);
+
+final TextStyle greyBold14 = TextStyle(
+  fontSize: 14,
+  color: Colors.grey[600],
+  fontWeight: FontWeight.bold,
+);
+
+final TextStyle greyBold16 = TextStyle(
+  fontSize: 16,
+  color: Colors.grey[600],
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle white8 = TextStyle(
+  fontSize: 8,
+  color: Colors.white,
+);
+
+const TextStyle white10 = TextStyle(
+  fontSize: 12,
+  color: Colors.white,
+);
+
+const TextStyle white12 = TextStyle(
+  fontSize: 12,
+  color: Colors.white,
+);
+
+const TextStyle white14 = TextStyle(
+  fontSize: 14,
+  color: Colors.white,
+);
+
+const TextStyle whiteBold = TextStyle(
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+const TextStyle whiteBold10 = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+const TextStyle whiteBold12 = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);

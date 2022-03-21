3c610084d6d0f091e9be80da9c3691f37c561dcd
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-03-21 14:17:32 +0900

    feat: add new tab to main page bottom navigation

--- packages/mobile/lib/route/routes.dart
+++ packages/mobile/lib/route/routes.dart
@@ -4,6 +4,7 @@ import '../pages/account/account_page.dart';
 import '../pages/home/home_page.dart';
 import '../pages/main/main_page.dart';
 import '../pages/map/map_page.dart';
+import '../pages/message/message_page.dart';
 import '../pages/not_found/not_found_page.dart';
 import '../pages/notification/notification.dart';
 import '../pages/second/second_page.dart';
@@ -14,6 +15,7 @@ final routeBuilder = <String, PageBuilder>{
   MainPage.path: (_, args) => const MainPage(key: ValueKey(MainPage.name)),
   HomePage.path: (_, args) => const HomePage(key: ValueKey(HomePage.name)),
   MapPage.path: (_, args) => const MapPage(key: ValueKey(MapPage.name)),
+  MessagePage.path: (_, args) => const MessagePage(key: ValueKey(MessagePage.name)),
   AccountPage.path: (_, args) => const AccountPage(key: ValueKey(AccountPage.name)),
   NotificationPage.path: (_, args) => const NotificationPage(key: ValueKey(NotificationPage.name)),
   NotFoundPage.path: (_, args) => const NotFoundPage(key: ValueKey(NotFoundPage.name)),

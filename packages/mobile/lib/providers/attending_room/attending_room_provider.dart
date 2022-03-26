a1d1db16bd5eca8c9934aee0f5b7c5f8e54857be
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-03-26 16:53:33 +0900

    feat: move unreadCountStreamProvider to attending_room_provider

--- packages/mobile/lib/providers/attending_room/attending_room_provider.dart
+++ packages/mobile/lib/providers/attending_room/attending_room_provider.dart
@@ -2,6 +2,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mottai_flutter_app_models/models.dart';
 
 import '../auth/auth_providers.dart';
+import '../room/room_provider.dart';
 
 /// ユーザーの attendingRoom コレクションを購読する StreamProvider
 final attendingRoomsStreamProvider = StreamProvider.autoDispose<List<AttendingRoom>>((ref) {
@@ -10,4 +11,29 @@ final attendingRoomsStreamProvider = StreamProvider.autoDispose<List<AttendingRo
     throw const SignInRequiredException();
   }
   return MessageRepository.subscribeAttendingRooms(userId: userId);
+});
+
+/// 指定した roomId の messages サブコレクションに、指定した DateTime より
+/// createdAt が未来の相手が送信した（送信者が自分ではない）
+/// ドキュメントの個数（最大 9 個）を購読する StreamProvider
+final unreadCountStreamProvider = StreamProvider.autoDispose.family<int, String>((ref, roomId) {
+  final userId = ref.watch(userIdProvider).value;
+  if (userId == null) {
+    throw const SignInRequiredException();
+  }
+  final room = ref.watch(roomStreamProvider(roomId)).value;
+  if (room == null) {
+    throw Exception('指定したルームが見つかりませんでした。');
+  }
+  final lastReadAt = room.lastReadAt[userId];
+  if (lastReadAt == null) {
+    return Stream.value(0);
+  }
+  return MessageRepository.subscribeMessages(
+    roomId: room.roomId,
+    queryBuilder: (q) => q
+        .where('createdAt', isGreaterThan: lastReadAt)
+        .orderBy('createdAt', descending: true)
+        .limit(10),
+  ).map((messages) => messages.where((message) => message.senderId != userId).toList().length);
 });

3a703d8046aa738d165e3bedd862daff7d2d0c23
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-04-02 17:44:48 +0900

    feat: add read/unread display on room page feature

--- packages/mobile/lib/providers/read_status/read_status_providers.dart
+++ packages/mobile/lib/providers/read_status/read_status_providers.dart
@@ -3,7 +3,8 @@ import 'package:mottai_flutter_app_models/models.dart';
 
 import '../providers.dart';
 
-/// 指定した roomId の Room ドキュメントを購読する StreamProvider
+/// 指定した roomId の rooms/{roomId}/readStatus/{自分の userId}
+/// のドキュメントを購読する StreamProvider。
 final readStatusStreamProvider =
     StreamProvider.autoDispose.family<ReadStatus?, String>((ref, roomId) {
   final userId = ref.watch(userIdProvider).value;
@@ -11,4 +12,19 @@ final readStatusStreamProvider =
     throw const SignInRequiredException();
   }
   return MessageRepository.subscribeReadStatus(roomId: roomId, readStatusId: userId);
+});
+
+/// 指定した roomId の相手が最後にメッセージ読んだ時間を購読する StreamProvider。
+final partnerReadStatusStreamProvider =
+    StreamProvider.autoDispose.family<ReadStatus?, String>((ref, roomId) {
+  final userId = ref.watch(userIdProvider).value;
+  if (userId == null) {
+    throw const SignInRequiredException();
+  }
+  final room = ref.watch(roomStreamProvider(roomId)).value;
+  if (room == null) {
+    throw Exception('指定したルームが見つかりませんでした。');
+  }
+  final partnerId = [room.workerId, room.hostId].firstWhere((id) => id != userId);
+  return MessageRepository.subscribeReadStatus(roomId: roomId, readStatusId: partnerId);
 });

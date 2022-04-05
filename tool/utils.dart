5763c5d74de7eb92bdf24659d70c8cbc6a96dded
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-04-05 20:01:09 +0900

    feat: add draft grinder setup

---
+++ tool/utils.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+
+///
+enum Flavor { local, dev, prod }
+
+/// モバイルアプリ
+class MobileAppTool {
+  static const relativeRootDir = 'packages/mobile/';
+  static final rootDir = joinAll([Directory.current.path, relativeRootDir]);
+  static final libDir = '${rootDir}lib/';
+  static final iosDir = '${rootDir}ios/';
+  static final androidDir = '${rootDir}android/';
+  static final entryPointPath = '${rootDir}lib/main.dart';
+
+  static const baseAppName = 'NPO MOTTAI';
+  static String ipaName(Flavor flavor) => '$baseAppName.${flavor.name}.ipa';
+  static String ipaPath(Flavor flavor) => '${rootDir}build/ios/ipa/${ipaName(flavor)}';
+
+  static final exportOptionsPath = '${iosDir}ExportOptions.plist';
+}
+
+/// ビルド番号
+Future<String> get buildNumber async {
+  final result = await Process.run(
+    'git',
+    ['rev-list', '--all', '--count'],
+    runInShell: true,
+  );
+  return (int.parse(result.stdout) + 10000).toString();
+}
+
+///
+Future<ProcessResult> shell(
+  String executable,
+  List<String> arguments,
+) async {
+  return Process.run(
+    executable,
+    arguments,
+    runInShell: true,
+  );
+}

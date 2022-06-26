aed72758a08ff9f721fecbe3555afb05a0dc89ac
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-06-26 16:10:28 +0900

    chore: add comment

--- packages/mobile/lib/providers/bottom_tab/bottom_tab.dart
+++ packages/mobile/lib/providers/bottom_tab/bottom_tab.dart
@@ -2,4 +2,5 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../route/bottom_tabs.dart';
 
+/// 現在選択状態になっている下タブを管理する StateProvider。
 final bottomTabStateProvider = StateProvider<BottomTab>((_) => bottomTabs[0]);

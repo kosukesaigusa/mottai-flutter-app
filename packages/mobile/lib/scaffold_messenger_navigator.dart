b62bc2d993972d03d13451d755dea854ee7e18ca
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-06-26 16:14:33 +0900

    feat: update scaffold messenger navigator widget

--- packages/mobile/lib/scaffold_messenger_navigator.dart
+++ packages/mobile/lib/scaffold_messenger_navigator.dart
@@ -4,40 +4,46 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'pages/not_found/not_found_page.dart';
 import 'providers/overlay_loading/overlay_loading.dart';
-import 'route/router.dart';
-import 'route/routes.dart';
+import 'route/app_router.dart';
+import 'services/navigation.dart';
 import 'services/scaffold_messenger_service.dart';
 import 'widgets/loading/loading.dart';
 
 /// Widget Tree の最上部で ScaffoldMessenger を含めるための Navigator ウィジェット。
 /// 目には見えないが、アプリケーション上の全てのページがこの Scaffold の上に載るので
-/// ScaffoldMessengerController でどこからでもスナックバーが表示できるようになっている。
+/// ScaffoldMessengerService でどこからでもスナックバーが表示できるようになっている。
 class ScaffoldMessengerNavigator extends HookConsumerWidget {
-  const ScaffoldMessengerNavigator({Key? key}) : super(key: key);
+  const ScaffoldMessengerNavigator({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return KeyboardVisibilityProvider(
-      child: ScaffoldMessenger(
-        key: ref.watch(scaffoldMessengerServiceProvider.select((c) => c.scaffoldMessengerKey)),
-        child: Scaffold(
-          body: Stack(
-            children: [
-              Navigator(
-                key: ref.watch(scaffoldMessengerServiceProvider.select((c) => c.navigatorKey)),
-                initialRoute: ref.watch(routerProvider(appRoutes)).initialRoute,
-                onGenerateRoute: ref.watch(routerProvider(appRoutes)).onGenerateRoute,
-                observers: const [],
-                onUnknownRoute: (settings) {
-                  final route = MaterialPageRoute<void>(
-                    settings: settings,
-                    builder: (context) => const NotFoundPage(),
-                  );
-                  return route;
-                },
+    return WillPopScope(
+      onWillPop: ref.read(navigationServiceProvider).maybePop,
+      child: KeyboardVisibilityProvider(
+        child: ScaffoldMessenger(
+          key: ref.watch(scaffoldMessengerServiceProvider.select((c) => c.scaffoldMessengerKey)),
+          child: GestureDetector(
+            onTap: () => FocusScope.of(context).unfocus(),
+            child: Scaffold(
+              body: Stack(
+                children: [
+                  Navigator(
+                    key: ref.watch(scaffoldMessengerServiceProvider.select((c) => c.navigatorKey)),
+                    initialRoute: ref.watch(appRouterProvider).initialRoute,
+                    onGenerateRoute: ref.watch(appRouterProvider).onGenerateRoute,
+                    onUnknownRoute: (settings) {
+                      final route = MaterialPageRoute<void>(
+                        settings: settings,
+                        builder: (context) => const NotFoundPage(),
+                      );
+                      return route;
+                    },
+                  ),
+                  if (ref.watch(overlayLoadingProvider)) const OverlayLoadingWidget(),
+                  // const ForceUpdateWidget(),
+                ],
               ),
-              if (ref.watch(overlayLoadingProvider)) const OverlayLoadingWidget(),
-            ],
+            ),
           ),
         ),
       ),

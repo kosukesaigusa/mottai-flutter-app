23eef07de8175f6ae49dc5182ce913c285a6f21c
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-06-26 16:24:49 +0900

    feat: add map extension method

--- packages/mobile/lib/utils/extensions/map.dart
+++ packages/mobile/lib/utils/extensions/map.dart
@@ -1,27 +1,14 @@
-extension MapExtension on Map<String, dynamic>? {
-  /// Map<String, dynamic>? から特定の Key の Value を取得する
-  ///
-  /// - this のインスタンスが null の場合は null
-  /// - [ignoreError] が true の場合 null
-  /// - [ignoreError] が false の場合 Exception
-  /// - キャストできない場合は null
-  ///
-  /// が返される。
-  T? getByKey<T extends Object?>(String key, {bool ignoreError = true}) {
-    if (this == null) {
-      return null;
-    }
-    if (!this!.containsKey(key)) {
-      if (ignoreError) {
-        return null;
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+extension MapExtension on Map<String, dynamic> {
+  Map<String, dynamic> toFirestore([String updatedAtFieldName = 'updatedAt']) {
+    final nonNullValueMap = <String, dynamic>{};
+    forEach((key, dynamic value) {
+      if (value != null) {
+        nonNullValueMap[key] = value;
       }
-      throw Exception('MapExtensionError: $key does not exist.');
-    }
-    try {
-      return this![key] as T;
-      // ignore: avoid_catching_errors
-    } on Error {
-      return null;
-    }
+    });
+    nonNullValueMap[updatedAtFieldName] = FieldValue.serverTimestamp();
+    return nonNullValueMap;
   }
 }

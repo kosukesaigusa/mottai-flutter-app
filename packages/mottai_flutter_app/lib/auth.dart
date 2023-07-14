1afaf5b9069677e41903b1db8b84df97d64ee0e3
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-07-14 15:46:56 +0900

    feat: add auth related providers

---
+++ packages/mottai_flutter_app/lib/auth.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+/// [FirebaseAuth] のインスタンスを提供する [Provider].
+final _authProvider = Provider<FirebaseAuth>((_) => FirebaseAuth.instance);
+
+/// [FirebaseAuth] の [User] を返す [StreamProvider].
+/// ユーザーの認証状態が変更される（ログイン、ログアウトする）たびに更新される。
+final authUserProvider = StreamProvider<User?>(
+  (ref) => ref.watch(_authProvider).userChanges(),
+);
+
+/// 現在のユーザー ID を提供する [Provider].
+/// [authUserProvider] の変更を watch しているので、ユーザーの認証状態が変更され
+/// るたびに、この [Provider] も更新される。
+final userIdProvider = Provider<String?>((ref) {
+  ref.watch(authUserProvider);
+  return ref.watch(_authProvider).currentUser?.uid;
+});
+
+/// ユーザーがログインしているかどうかを示す bool 値を提供する Provider.
+/// [userIdProvider] の変更を watch しているので、ユーザーの認証状態が変更され
+/// るたびに、この [Provider] も更新される。
+final isSignedInProvider = Provider<bool>(
+  (ref) => ref.watch(userIdProvider) != null,
+);

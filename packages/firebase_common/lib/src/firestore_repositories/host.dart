e7e2c54c0458045f6ac1b645d2b755fae7415e48
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-07-17 18:24:11 +0900

    feat: add methods to host and worker repositories

--- packages/firebase_common/lib/src/firestore_repositories/host.dart
+++ packages/firebase_common/lib/src/firestore_repositories/host.dart
@@ -2,4 +2,12 @@ import '../firestore_documents/host.dart';
 
 class HostRepository {
   final _query = HostQuery();
+
+  /// 指定した [Host] を購読する。
+  Stream<ReadHost?> subscribeHost({required String hostId}) =>
+      _query.subscribeDocument(hostId: hostId);
+
+  /// 指定した [Host] を取得する。
+  Future<ReadHost?> fetchHost({required String hostId}) =>
+      _query.fetchDocument(hostId: hostId);
 }
--- packages/firebase_common/lib/src/firestore_repositories/worker.dart
+++ packages/firebase_common/lib/src/firestore_repositories/worker.dart
@@ -2,4 +2,8 @@ import '../firestore_documents/worker.dart';
 
 class WorkerRepository {
   final _query = WorkerQuery();
+
+  /// 指定した [Worker] を購読する。
+  Stream<ReadWorker?> subscribeWorker({required String workerId}) =>
+      _query.subscribeDocument(workerId: workerId);
 }

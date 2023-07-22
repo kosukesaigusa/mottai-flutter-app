0ff27aa56692c8b94dace6ed1d8812eb984ebca5
haterain0203 <haterain0203@gmail.com>
2023-07-22 15:27:08 +0900

    StatelessWidgetの大枠追加

--- packages/dart_flutter_common/lib/src/components/image.dart
+++ packages/dart_flutter_common/lib/src/components/image.dart
@@ -1,5 +1,16 @@
+import 'package:flutter/material.dart';
+
 enum ImageShape {
   circle,
   square,
   rectangle,
+}
+
+class GenericImageWidget extends StatelessWidget {
+  const GenericImageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }

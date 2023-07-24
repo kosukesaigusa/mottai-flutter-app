9e0ccb577369e941dee9256cf7ab40d84a161068
小池俊輔 <Koike.s0731@icloud.com>
2023-07-24 18:49:15 +0900

    画像が選択されなかった時に例外ではなくnullを返すように変更

--- packages/mottai_flutter_app/lib/development/image_pick/image_pick_function.dart
+++ packages/mottai_flutter_app/lib/development/image_pick/image_pick_function.dart
@@ -1,29 +1,37 @@
+// ignore_for_file: avoid_catches_without_on_clauses
+
 import 'dart:io';
 
 import 'package:image_picker/image_picker.dart';
-
 
 enum PickType {
   camera,
   gallery,
 }
+
 final ImagePicker _picker = ImagePicker();
 
 ///一枚のみ選択する場合
 ///写真を撮影するか選択するかはenumのPickTypeで渡す
-Future<File> fetchSingleImage(PickType pickType) async {
-  final _imageFile = await _picker.pickImage(
-    source:
-        pickType == PickType.camera ? ImageSource.camera : ImageSource.gallery,
-  );
-  if (_imageFile == null) {
-    throw Exception('画像が選択されませんでした。');
+Future<File?> fetchSingleImage(PickType pickType) async {
+  try {
+    final _imageFile = await _picker.pickImage(
+      source: pickType == PickType.camera
+          ? ImageSource.camera
+          : ImageSource.gallery,
+    );
+    return _imageFile == null ? null : File(_imageFile.path);
+  } catch (e) {
+    return null;
   }
-  return File(_imageFile.path);
 }
 
 ///複数選択する場合
 Future<List<File>> fetchMultipleImage() async {
-  final _imageFiles = await _picker.pickMultiImage();
-  return _imageFiles.map((e) => File(e.path)).toList();
+  try {
+    final _imageFiles = await _picker.pickMultiImage();
+    return _imageFiles.map((e) => File(e.path)).toList();
+  } catch (e) {
+    return [];
+  }
 }

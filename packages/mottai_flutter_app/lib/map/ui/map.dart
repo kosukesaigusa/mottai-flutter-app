fb15cd9b157e128e92afe27b70efe4030631e341
HiroMinase <lostsymbol612@gmail.com>
2023-07-24 20:40:23 +0900

    line length が 80 なので改行を追加

--- packages/mottai_flutter_app/lib/map/ui/map.dart
+++ packages/mottai_flutter_app/lib/map/ui/map.dart
@@ -32,7 +32,11 @@ class MapPage extends ConsumerWidget {
               alignment: Alignment.bottomCenter,
               child: GestureDetector(
                 onTap: () {
-                  ref.read(currentLocationControllerProvider).getCurrentPosition();
+                  ref
+                      .read(
+                        currentLocationControllerProvider,
+                      )
+                      .getCurrentPosition();
                 },
                 child: Container(
                   padding: const EdgeInsets.symmetric(
@@ -56,7 +60,9 @@ class MapPage extends ConsumerWidget {
                         ),
                       ),
                       ref.watch(currentPositionProvider).when(
-                            data: (position) => Text('緯度: ${position.latitude}, 経度: ${position.longitude}'),
+                            data: (position) => Text(
+                              '緯度: ${position.latitude}, 経度: ${position.longitude}',
+                            ),
                             error: (_, __) => const SizedBox(),
                             loading: () => const Center(
                               child: CircularProgressIndicator(),

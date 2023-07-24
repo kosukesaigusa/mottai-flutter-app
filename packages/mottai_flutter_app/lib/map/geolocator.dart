6003e4b2f744d848d5a9512ea2690f1ffe0b0b30
Hiro <31511218+minasehiro@users.noreply.github.com>
2023-07-24 22:11:22 +0900

    現在地を取得できるように (#70)
    * 現在地を取得するボタンを作成
    
    * view から呼ばれる controller を作成
    
    * geolocator を使って現在地を取得するメソッドを作成
    
    * iOSの現在地取得権限を得る
    
    * ビルド時に必要だった pod install --repo-update による firebase_storage の更新
    
    * コメントを調整
    
    * コメントを調整
    
    * コメントを削除
    
    * 不要な変数定義を削除しコメントの記法を調整
    
    * Position? を返すように
    
    * line length が 80 なので改行を追加
    
    * コメントの記法を修正
    
    * 改行を追加
    
    * line length に対応するためテンプレートリテラルを使用
    
    * if 文の書き方を変更

---
+++ packages/mottai_flutter_app/lib/map/geolocator.dart
@@ -0,0 +1,28 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final locationServiceProvider = Provider((ref) => LocationService());
+
+/// geolocator で取得した [Position] を返す [FutureProvider].
+/// 取得したユーザーの現在の緯度経度が保存される
+final currentPositionProvider = FutureProvider<Position>((ref) {
+  return ref.watch(locationServiceProvider).getCurrentPosition();
+});
+
+class LocationService {
+  /// 現在地を取得するための権限を取得
+  Future<LocationPermission> getLocationPermission() async {
+    return Geolocator.requestPermission();
+  }
+
+  /// 現在地を取得
+  Future<Position> getCurrentPosition() async {
+    if (await Geolocator.isLocationServiceEnabled()) {
+      return Future.error('位置情報サービスが使えない状態です');
+    }
+
+    return Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+  }
+}
--- packages/mottai_flutter_app/lib/map/ui/map.dart
+++ packages/mottai_flutter_app/lib/map/ui/map.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class MapPage extends StatelessWidget {
+import '../geolocator.dart';
+import 'geolocator_controller.dart';
+
+class MapPage extends ConsumerWidget {
   const MapPage({super.key});
 
   static const path = '/map';
@@ -9,13 +13,68 @@ class MapPage extends StatelessWidget {
   static const location = path;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: const Text('マップ')),
-      body: const GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(35.681236, 139.767125),
-        ),
+      body: Stack(
+        children: [
+          const GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: LatLng(35.681236, 139.767125),
+            ),
+          ),
+          // 仮ボタン
+          Container(
+            margin: EdgeInsets.only(
+              bottom: MediaQuery.of(context).size.height * 0.1,
+            ),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: GestureDetector(
+                onTap: () {
+                  ref
+                      .read(
+                        currentLocationControllerProvider,
+                      )
+                      .getCurrentPosition();
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 5,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    border: Border.all(
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        '現在地を取得',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      ref.watch(currentPositionProvider).when(
+                            data: (position) => Text('''
+                              緯度: ${position.latitude}, 経度: ${position.longitude}
+                            '''),
+                            error: (_, __) => const SizedBox(),
+                            loading: () => const Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }

c18eb315981b1c9e54c1b35443a04bd3fecb6ba5
naokiwakata <rockwakata15@gmail.com>
2023-07-26 22:44:24 +0900

    FirebaseStorageServiceクラスをミニマムで実装

--- packages/firebase_common/lib/src/firebase_storage/firebase_storage.dart
+++ packages/firebase_common/lib/src/firebase_storage/firebase_storage.dart
@@ -1,1 +1,15 @@
+import 'dart:io';
 
+import 'package:firebase_storage/firebase_storage.dart';
+
+class FirebaseStorageService {
+  FirebaseStorageService({required String path}) {
+    _reference = FirebaseStorage.instance.ref().child(path);
+  }
+  late Reference _reference;
+
+  Future<String> upload(File file) async {
+    await _reference.putFile(file);
+    return _reference.getDownloadURL();
+  }
+}

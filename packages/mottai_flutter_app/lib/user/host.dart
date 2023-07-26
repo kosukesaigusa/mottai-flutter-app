6c44325f0bcaca0a031a11384ebd4c532aad113c
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-07-26 17:02:47 +0900

    feat: add host service and add hostExists method

--- packages/mottai_flutter_app/lib/user/host.dart
+++ packages/mottai_flutter_app/lib/user/host.dart
@@ -1,1 +1,23 @@
+import 'package:firebase_common/firebase_common.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../firestore_repository.dart';
+
+final hostServiceProvider = Provider.autoDispose<HostService>(
+  (ref) => HostService(
+    hostRepository: ref.watch(hostRepositoryProvider),
+  ),
+);
+
+class HostService {
+  const HostService({required HostRepository hostRepository})
+      : _hostRepository = hostRepository;
+
+  final HostRepository _hostRepository;
+
+  /// 指定した [Host] が存在するかどうかを返す。
+  Future<bool> hostExists({required String hostId}) async {
+    final host = await _hostRepository.fetchHost(hostId: hostId);
+    return host != null;
+  }
+}

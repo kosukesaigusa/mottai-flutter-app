cc01274dd625bd234033903ae5c97d79422fa752
narunblog <crazycat0727@gmail.com>
2023-08-15 01:14:09 +0900

    fix analyze lines_longer_than_80_chars

--- packages/mottai_flutter_app/lib/host/ui/host.dart
+++ packages/mottai_flutter_app/lib/host/ui/host.dart
@@ -10,6 +10,7 @@ import '../../job/job.dart';
 import '../../user/host.dart';
 import '../../user/ui/identity_dependent_builder.dart';
 import '../../user/ui/user_mode.dart';
+import '../../user/user_mode.dart';
 import 'create_or_update_host.dart';
 
 /// ホストページ。
@@ -51,6 +52,7 @@ class HostPageBody extends ConsumerWidget {
     final hostImageUrl = ref.watch(hostImageUrlProvider(userId));
     final hostDisplayName = ref.watch(hostDisplayNameProvider(userId));
     final readHost = ref.watch(hostFutureProvider(userId));
+    final currentUserMode = ref.watch(userModeStateProvider);
     return SingleChildScrollView(
       // TODO: Divider は横いっぱいに表示したいので Padding の水平方向の全体適用はやめたい。
       child: Padding(
@@ -159,13 +161,114 @@ class HostPageBody extends ConsumerWidget {
                 ],
               ),
             ),
+            IdentityDependentBuilder(
+              buildForIdentity: () {
+                return Column(
+                  children: [
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Section(
+                      titleBottomMargin: 4,
+                      title: 'ユーザーモード',
+                      content: Text(
+                        currentUserMode == UserMode.host
+                            ? '''
+ホストとしてアプリを使用します。あなたが募集するお手伝いに興味があるワーカーとやりとりをして、お手伝いを受け入れるモードです。'''
+                            : '''ワーカーとしてアプリを使用します。興味のあるホストやお手伝いを探して、お手伝いに応募するモードです。''',
+                      ),
+                    ),
+                    SegmentedButton<UserMode>(
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                      ),
+                      segments: const <ButtonSegment<UserMode>>[
+                        ButtonSegment<UserMode>(
+                          value: UserMode.host,
+                          label: Text('ホスト'),
+                        ),
+                        ButtonSegment<UserMode>(
+                          value: UserMode.worker,
+                          label: Text('ワーカー'),
+                        ),
+                      ],
+                      selected: <UserMode>{currentUserMode},
+                      onSelectionChanged: (newSelection) {
+                        ref
+                            .read(userModeStateProvider.notifier)
+                            .update((_) => newSelection.first);
+                      },
+                    ),
+                  ],
+                );
+              },
+              targetUserId: userId,
+            ),
+
+            const SizedBox(height: 24),
+            // TODO 自己紹介をDBに追加する
+            const Section(
+              titleBottomMargin: 4,
+              title: '自己紹介',
+              content: Text(
+                '''
+神奈川県小田原市で農家や漁師をしています。夏の時期にレモンの収穫のお手伝いをしてくれる方を募集しています。こんな感じでここには自己紹介文を表示する。表示するのは最大 8 行表くらいでいいだろうか。あいうえお、かきくけこ、さしすせそ、たちつてと、なにぬねの、はひふへほ、まみむめも、やゆよ、わをん、あいうえお、かきくけこ、さしすせそ、たちつてと、なにぬねの、はひふへほ、まみむめも、やゆよ...''',
+              ),
+            ),
+            const SizedBox(height: 24),
+            const Section(
+              titleBottomMargin: 4,
+              title: 'ホストタイプ',
+              content: Text('ワーカーはホストタイプ（複数選択可）を参考にして、興味のあるお手伝いを探します。'),
+            ),
+            readHost.when(
+              data: (readHost) {
+                if (readHost == null) {
+                  return const Center(
+                    child: Text('ホストタイプがありません。'),
+                  );
+                }
+                return SelectableChips<HostType>(
+                  allItems: HostType.values,
+                  labels: Map.fromEntries(
+                    HostType.values.map(
+                      (type) => MapEntry(type, type.label),
+                    ),
+                  ),
+                  enabledItems: readHost.hostTypes,
+                );
+              },
+              loading: () => const Center(child: CircularProgressIndicator()),
+              error: (error, stackTrace) => const Center(
+                child: Text('通信に失敗しました。'),
+              ),
+            ),
+            const SizedBox(height: 24),
+            const Section(
+              titleBottomMargin: 4,
+              title: '公開する場所・住所',
+              content: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('''
+農場や主な作業場所などの、公開される場所・住所です。ワーカーは地図上から近所や興味がある地域のホストを探します。必ずしも正確で細かい住所である必要はありません。'''),
+                  SizedBox(height: 12),
+                  // TODO ここは後でデータを取得する
+                  Text('神奈川県小田原市石322 (hostLocation.address)'),
+                ],
+              ),
+            ),
             const Divider(
               height: 36,
             ),
             Section(
+              titleBottomMargin: 4,
               title: '掲載中のお手伝い募集',
-              titleStyle: Theme.of(context).textTheme.titleLarge,
-              titleBottomMargin: 8,
               content: ref.watch(userJobsFutureProvider(userId)).when(
                     data: (jobs) {
                       if (jobs.isEmpty) {

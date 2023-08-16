8d2a25d1e45d83a8792eaaeff3f35cdef3468e8b
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-08-16 10:18:40 +0900

    feat: show worker or host content on my account page

--- packages/mottai_flutter_app/lib/my_account/ui/my_account.dart
+++ packages/mottai_flutter_app/lib/my_account/ui/my_account.dart
@@ -1,9 +1,15 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../auth/ui/auth_dependent_builder.dart';
+import '../../host/ui/host.dart';
+import '../../user/user_mode.dart';
+import '../../worker/ui/worker.dart';
 
 /// マイアカウントページ。中身は Worker や Host の詳細画面を構成するウィジェットを使用する。
 @RoutePage()
-class MyAccountPage extends StatelessWidget {
+class MyAccountPage extends ConsumerWidget {
   const MyAccountPage({super.key});
 
   /// [AutoRoute] で指定するパス文字列。
@@ -13,7 +19,17 @@ class MyAccountPage extends StatelessWidget {
   static const location = path;
 
   @override
-  Widget build(BuildContext context) {
-    return const SizedBox();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userMode = ref.watch(userModeStateProvider);
+    return AuthDependentBuilder(
+      onAuthenticated: (userId) {
+        switch (userMode) {
+          case UserMode.worker:
+            return WorkerPageBody(userId: userId);
+          case UserMode.host:
+            return HostPageBody(userId: userId);
+        }
+      },
+    );
   }
 }
---
+++ packages/mottai_flutter_app/lib/user/ui/user_mode.dart
@@ -0,0 +1,59 @@
+import 'package:dart_flutter_common/dart_flutter_common.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../user_mode.dart';
+
+/// ワーカーページ、ホストページなどで使用する、[UserMode] を選択する [Section].
+class UserModeSection extends ConsumerWidget {
+  const UserModeSection({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userMode = ref.watch(userModeStateProvider);
+    // TODO:「自分かどうか Builder」を使用する。
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Section(
+          titleBottomMargin: 8,
+          title: 'ユーザーモード',
+          titleStyle: Theme.of(context).textTheme.titleLarge,
+          content: Text(
+            userMode == UserMode.host
+                ? '''
+ホストとしてアプリを使用します。あなたが募集するお手伝いに興味があるワーカーとやりとりをして、お手伝いを受け入れるモードです。'''
+                : 'ワーカーとしてアプリを使用します。興味のあるホストやお手伝いを探して、お手伝いに応募するモードです。',
+          ),
+        ),
+        const Gap(8),
+        SegmentedButton<UserMode>(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+          ),
+          segments: const <ButtonSegment<UserMode>>[
+            ButtonSegment<UserMode>(
+              value: UserMode.host,
+              label: Text('ホスト'),
+            ),
+            ButtonSegment<UserMode>(
+              value: UserMode.worker,
+              label: Text('ワーカー'),
+            ),
+          ],
+          selected: <UserMode>{userMode},
+          onSelectionChanged: (newSelection) {
+            ref
+                .read(userModeStateProvider.notifier)
+                .update((_) => newSelection.first);
+          },
+        )
+      ],
+    );
+  }
+}

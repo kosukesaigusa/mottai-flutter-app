c9f183a4c19f8fa02d4093fccb83a8fe66916be7
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-08-29 09:29:46 +0900

    fix: fix codes

--- packages/mottai_flutter_app/lib/chat/ui/chat_rooms.dart
+++ packages/mottai_flutter_app/lib/chat/ui/chat_rooms.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:dart_flutter_common/dart_flutter_common.dart';
-import 'package:firebase_common/firebase_common.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -62,31 +61,19 @@ class ChatRoomsPage extends ConsumerWidget {
               return ListView.builder(
                 itemCount: chatRooms.length,
                 itemBuilder: (context, index) {
-                  final readChatRoom = chatRooms[index];
-                  // Records でchatRoomで扱いたいデータをまとめた
-                  final chatRoom = (
-                    id: readChatRoom.chatRoomId,
-                    chatPartnerImageUrl: ref.watch(
-                      chatPartnerImageUrlProvider(readChatRoom),
+                  final chatRoom = chatRooms[index];
+                  final latestChatMessage =
+                      ref.watch(latestMessageProvider(chatRoom.chatRoomId));
+                  return GenericChatRoom(
+                    imageUrl: ref.watch(chatPartnerImageUrlProvider(chatRoom)),
+                    title: ref.watch(chatPartnerDisplayNameProvider(chatRoom)),
+                    latestMessage: latestChatMessage?.content,
+                    onTap: () => context.router.pushNamed(
+                      ChatRoomPage.location(chatRoomId: chatRoom.chatRoomId),
                     ),
-                    chatPartnerDisplayName: ref.watch(
-                      chatPartnerDisplayNameProvider(
-                        readChatRoom,
-                      ),
-                    ),
-                    latestChatMessage: ref.watch(
-                      latestMessageProvider(readChatRoom.chatRoomId),
-                    ),
-                    unReadCountString: ref.watch(
-                      unReadCountStringProvider(readChatRoom),
-                    )
-                  );
-                  return _ChatRoomListItem(
-                    chatRoomId: chatRoom.id,
-                    chatPartnerImageUrl: chatRoom.chatPartnerImageUrl,
-                    latestChatMessage: chatRoom.latestChatMessage,
-                    chatPartnerDisplayName: chatRoom.chatPartnerDisplayName,
-                    unReadCountString: chatRoom.unReadCountString,
+                    updatedAt: latestChatMessage?.createdAt,
+                    unReadCountString:
+                        ref.watch(unReadCountStringProvider(chatRoom)),
                   );
                 },
               );
@@ -95,55 +82,6 @@ class ChatRoomsPage extends ConsumerWidget {
             error: (_, __) => const SizedBox(),
           );
         },
-      ),
-    );
-  }
-}
-
-/// `ChatRoomListTile` を使ってmottaiアプリ用に作った ListItem
-class _ChatRoomListItem extends StatelessWidget {
-  const _ChatRoomListItem({
-    required this.chatRoomId,
-    required this.chatPartnerImageUrl,
-    required this.latestChatMessage,
-    required this.chatPartnerDisplayName,
-    required this.unReadCountString,
-  });
-
-  /// chatRoomId
-  final String chatRoomId;
-
-  /// チャット相手の画像
-  final String chatPartnerImageUrl;
-
-  /// 未読メッセージのドキュメント
-  final ReadChatMessage? latestChatMessage;
-
-  /// チャット相手の名前
-  final String chatPartnerDisplayName;
-
-  /// 未読メッセージ数
-  final String unReadCountString;
-
-  @override
-  Widget build(BuildContext context) {
-    return ChatRoomListTile(
-      onTap: () => context.router.pushNamed(
-        ChatRoomPage.location(
-          chatRoomId: chatRoomId,
-        ),
-      ),
-      chatPartnerImageUrl: chatPartnerImageUrl,
-      latestChatMessageCreatedAt: latestChatMessage?.createdAt,
-      latestChatMessageContent: latestChatMessage?.content,
-      chatPartnerDisplayName: chatPartnerDisplayName,
-      unReadCountString: unReadCountString,
-      decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(
-            color: Colors.grey,
-          ),
-        ),
       ),
     );
   }

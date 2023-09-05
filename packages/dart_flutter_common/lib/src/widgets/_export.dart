a05374ef55bd9afa33289ca1de7c8d60adad2d36
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-09-05 09:14:48 +0900

    feat: add material vertical card file

--- packages/dart_flutter_common/lib/src/widgets/_export.dart
+++ packages/dart_flutter_common/lib/src/widgets/_export.dart
@@ -1,6 +1,7 @@
 export 'generic_chat_room.dart';
 export 'generic_image.dart';
 export 'material_horizontal_card.dart';
+export 'material_vertical_card.dart';
 export 'section.dart';
 export 'selectable_chips.dart';
 export 'web_link.dart';

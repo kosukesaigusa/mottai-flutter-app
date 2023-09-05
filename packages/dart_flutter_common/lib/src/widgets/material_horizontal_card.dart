02ecf7c2db020b741d05a0ca93321be61c2250b7
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-09-05 09:53:01 +0900

    feat: improve MaterialHorizontalCard component

--- packages/dart_flutter_common/lib/src/widgets/material_horizontal_card.dart
+++ packages/dart_flutter_common/lib/src/widgets/material_horizontal_card.dart
@@ -8,62 +8,110 @@ class MaterialHorizontalCard extends StatelessWidget {
   /// Material 3 のデザインに従って、タイトル、詳細、画像の 3 つを水平方向に表示するカード
   /// ウィジェット。
   const MaterialHorizontalCard({
-    required this.title,
-    required this.description,
-    this.imageUrl,
+    this.headerImageUrl,
+    required this.header,
+    required this.subhead,
+    this.subheadMaxLines = 1,
+    this.mediaImageUrl,
+    this.borderRadius = 8,
+    this.contentVerticalPadding = 16,
+    this.contentHorizontalPadding = 16,
+    this.headerImageRightPadding = 16,
+    this.headerImageSize = 40,
+    this.mediaImageSize = 80,
     super.key,
   });
 
-  /// カードに表示するタイトル。
-  final String title;
+  /// ヘッダー画像の URL 文字列。
+  final String? headerImageUrl;
 
-  /// カードに表示する詳細。
-  final String description;
+  /// ヘッダー文字列。
+  final String header;
 
-  /// 表示する画像の URL 文字列。
-  final String? imageUrl;
+  /// ヘッダー下に表示する文字列。
+  final String subhead;
+
+  /// ヘッダー下に表示する文字列の最大行数。
+  final int subheadMaxLines;
+
+  /// メディア画像の URL 文字列。
+  final String? mediaImageUrl;
 
   /// カードの角丸の半径。
-  static const double _borderRadius = 8;
+  final double borderRadius;
+
+  /// ヘッダー画像の右側の余白。
+  final double headerImageRightPadding;
+
+  /// コンテンツ部分（左側）の垂直方向の余白。
+  final double contentVerticalPadding;
+
+  /// コンテンツ部分（左側）の水平方向の余白。
+  final double contentHorizontalPadding;
+
+  /// ヘッダー画像のサイズ。
+  final double headerImageSize;
+
+  /// メディア画像のサイズ。
+  final double mediaImageSize;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.zero,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(_borderRadius),
+        borderRadius: BorderRadius.circular(borderRadius),
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Flexible(
+          Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              padding: EdgeInsets.symmetric(
+                vertical: contentVerticalPadding,
+                horizontal: contentHorizontalPadding,
+              ),
+              child: Row(
                 children: [
-                  Text(
-                    title,
-                    style: Theme.of(context).textTheme.titleMedium,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Text(
-                    description,
-                    style: Theme.of(context).textTheme.labelSmall,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
+                  if (headerImageUrl != null)
+                    Padding(
+                      padding: EdgeInsets.only(right: headerImageRightPadding),
+                      child: GenericImage.circle(
+                        imageUrl: headerImageUrl!,
+                        size: headerImageSize,
+                      ),
+                    ),
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          header,
+                          style: Theme.of(context).textTheme.titleMedium,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          subhead,
+                          style: Theme.of(context).textTheme.labelSmall,
+                          maxLines: subheadMaxLines,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
           ),
-          if (imageUrl != null)
+          if (mediaImageUrl != null)
             ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(_borderRadius),
-                bottomRight: Radius.circular(_borderRadius),
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(borderRadius),
+                bottomRight: Radius.circular(borderRadius),
               ),
-              child: GenericImage.square(imageUrl: imageUrl!, size: 96),
+              child: GenericImage.square(
+                imageUrl: mediaImageUrl!,
+                size: mediaImageSize,
+              ),
             ),
         ],
       ),

c85225a7331a430d433f43be59efe22dbc74e670
haterain0203 <haterain0203@gmail.com>
2023-09-07 12:14:08 +0900

    ドキュメントモデル修正に伴うRepositoryの修正

--- packages/firebase_common/lib/src/firestore_repositories/disable_user_account_request.dart
+++ packages/firebase_common/lib/src/firestore_repositories/disable_user_account_request.dart
@@ -1,15 +1,15 @@
 import '../firestore_documents/disable_user_account_request.dart';
 
 class DisableUserAccountRequestRepository {
-  final _query = DisableUserAccountRequestsQuery();
+  final _query = DisableUserAccountRequestQuery();
 
-  /// [DisableUserAccountRequests] を作成する。
+  /// [DisableUserAccountRequest] を作成する。
   Future<void> setDisableUserAccountRequest({
     required String userId,
   }) =>
       _query.set(
         disableUserAccountRequestId: userId,
-        createDisableUserAccountRequests: CreateDisableUserAccountRequests(
+        createDisableUserAccountRequest: CreateDisableUserAccountRequest(
           userId: userId,
         ),
       );

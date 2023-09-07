cea463e4ce473b93c4950a10b6b03763969c2600
haterain0203 <haterain0203@gmail.com>
2023-09-07 12:26:04 +0900

    firestore_repositoryの更新

--- packages/mottai_flutter_app/lib/firestore_repository.dart
+++ packages/mottai_flutter_app/lib/firestore_repository.dart
@@ -7,6 +7,11 @@ final chatMessageRepositoryProvider =
 final chatRoomRepositoryProvider =
     Provider.autoDispose<ChatRoomRepository>((_) => ChatRoomRepository());
 
+final disableUserAccountRequestRepositoryProvider =
+    Provider.autoDispose<DisableUserAccountRequestRepository>(
+  (_) => DisableUserAccountRequestRepository(),
+);
+
 final fcmTokenRepositoryProvider =
     Provider.autoDispose<FcmTokenRepository>((_) => FcmTokenRepository());
 

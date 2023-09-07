99e7ed7ec99d1c13ae3607f5a92fe6ee2bec3a28
haterain0203 <haterain0203@gmail.com>
2023-09-07 12:30:15 +0900

    Serviceクラスの更新

--- packages/mottai_flutter_app/lib/development/disable_user_account_request/disable_user_account_request.dart
+++ packages/mottai_flutter_app/lib/development/disable_user_account_request/disable_user_account_request.dart
@@ -1,14 +1,33 @@
+import 'package:firebase_common/firebase_common.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../firestore_repository.dart';
 
 final disableUserAccountRequestServiceProvider =
     Provider.autoDispose<DisableUserAccountRequestService>(
-  (ref) => DisableUserAccountRequestService(),
+  (ref) => DisableUserAccountRequestService(
+    disableUserAccountRequestRepository: ref.watch(
+      disableUserAccountRequestRepositoryProvider,
+    ),
+  ),
 );
 
 class DisableUserAccountRequestService {
+  const DisableUserAccountRequestService({
+    required DisableUserAccountRequestRepository
+        disableUserAccountRequestRepository,
+  }) : _disableUserAccountRequestRepository =
+            disableUserAccountRequestRepository;
 
-  /// 引数で受ける [userId] を用いて [disableUserAccountRequest] ドキュメントを作成する
-  Future<void> createDisableUserAccountRequest({required String userId}) async {
-    
+  final DisableUserAccountRequestRepository
+      _disableUserAccountRequestRepository;
+
+  /// 引数で受ける [userId] を用いて [DisableUserAccountRequest] ドキュメントを作成する
+  Future<void> createDisableUserAccountRequest({
+    required String userId,
+  }) async {
+    await _disableUserAccountRequestRepository.setDisableUserAccountRequest(
+      userId: userId,
+    );
   }
 }

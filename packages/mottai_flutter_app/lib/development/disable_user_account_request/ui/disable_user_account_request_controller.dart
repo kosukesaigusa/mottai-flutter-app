885cfde935c1bd899a5d0f24a3a64789e6d1609e
haterain0203 <haterain0203@gmail.com>
2023-09-11 13:49:14 +0900

    importすべきパッケージの修正

--- packages/mottai_flutter_app/lib/development/disable_user_account_request/ui/disable_user_account_request_controller.dart
+++ packages/mottai_flutter_app/lib/development/disable_user_account_request/ui/disable_user_account_request_controller.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
